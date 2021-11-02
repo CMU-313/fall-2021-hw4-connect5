f11fa5e68f8d546a5ca0868605f10ffb86cc9998
James Frucht <jfrucht25@gmail.com>
2021-11-01 22:35:29 -0400

    make api test actually test json api

--- test_api.py
+++ test_api.py
@@ -1,20 +1,24 @@
 import requests
+import json
 
 URL = 'http://localhost:5000/predict'
+URL_JSON = 'http://localhost:5000/predictjson'
 
 def test_empty_request():
     response = requests.get(URL)
     assert response.status_code == 500
 
 def test_url_request():
-    response = requests.get(URL+'?age=10&absences=5&health=1')
+    params = {'age' : 10, 'absences' : 5, 'health' : 1}
+    response = requests.get(URL, params=params)
     assert response.status_code == 200
     text = response.text.strip()
     assert text == '0' or text == '1'
 
 def test_api_request():
-    params = {'age' : 10, 'absences' : 5, 'health' : 1}
-    response = requests.get(URL, params=params)
+    data = {'age' : 10, 'absences' : 5, 'health' : 1}
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(URL_JSON, data=json.dumps(data), headers=headers)
     assert response.status_code == 200
     text = response.text.strip()
     assert text == '0' or text == '1'
