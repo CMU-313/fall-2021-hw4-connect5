95b3151567a075a161bc7c96d63cd75a1d86e921
James Frucht <jfrucht25@gmail.com>
2021-11-02 16:50:57 -0400

    adjust model goals

--- test_model.py
+++ test_model.py
@@ -9,22 +9,22 @@ def data():
 
 @pytest.fixture
 def model():
-    return joblib.load('dockerfile/apps/model.pkl')
+    return joblib.load('dockerfile/apps/improved_model.pkl')
 
 def test_f1score(model, data):
     x_test, y_test = data
     pred = model.predict(x_test)
     score = sklearn.metrics.f1_score(y_test, pred, average='binary')
-    assert score >= .8
+    assert score >= .9
 
 def test_accuray(model, data):
     x_test, y_test = data
     pred = model.predict(x_test)
     score = sklearn.metrics.accuracy_score(y_test, pred)
-    assert score >= .9
+    assert score >= .95
 
 def test_log_loss(model, data):
     x_test, y_test = data
     pred = model.predict(x_test)
     score = sklearn.metrics.log_loss(y_test, pred)
-    assert score <= 0.2
+    assert score <= 1.5
