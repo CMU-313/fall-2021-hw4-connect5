e2e54d1029bc94e8bc0585a01c03546a5a2a511f
James Frucht <jfrucht25@gmail.com>
2021-11-01 23:05:35 -0400

    add retries to api requests

--- test_api.py
+++ test_api.py
@@ -1,24 +1,36 @@
 import requests
 import json
+import pytest
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 URL = 'http://localhost:5000/predict'
 URL_JSON = 'http://localhost:5000/predictjson'
 
-def test_empty_request():
-    response = requests.get(URL)
+@pytest.fixture
+def http():
+    http = requests.Session()
+    retries = Retry(total=10,
+                backoff_factor=0.1)
+
+    http.mount('http://', HTTPAdapter(max_retries=retries))
+    return http
+
+def test_empty_request(http):
+    response = http.get(URL)
     assert response.status_code == 500
 
-def test_url_request():
+def test_url_request(http):
     params = {'age' : 10, 'absences' : 5, 'health' : 1}
-    response = requests.get(URL, params=params)
+    response = http.get(URL, params=params)
     assert response.status_code == 200
     text = response.text.strip()
     assert text == '0' or text == '1'
 
-def test_api_request():
+def test_api_request(http):
     data = {'age' : 10, 'absences' : 5, 'health' : 1}
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(URL_JSON, data=json.dumps(data), headers=headers)
+    response = http.post(URL_JSON, data=json.dumps(data), headers=headers)
     assert response.status_code == 200
     text = response.text.strip()
     assert text == '0' or text == '1'
